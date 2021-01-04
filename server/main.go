ce20cc314e2f38557bbc9f95dbb2d3d324e1e23b
tankz <1785619665@qq.com>
2021-01-04 16:44:36 +0800

    [add] 新增student的model

--- server/main.go
+++ server/main.go
@@ -1,14 +1,20 @@
 package main
 
-import "github.com/gin-gonic/gin"
-import "ccs/controller"
+import (
+	"ccs/config"
+	"ccs/controller"
+	"ccs/db"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	db.Init()
+	config.Init()
 	router := gin.Default()
 	v1 := router.Group("/api/student")
 	{
-		v1.POST("/login", controller.LoginHandler)
-		v1.POST("/register", controller.RegisterHandler)
+		v1.POST("/login", controller.StudentLoginHandler)
+		v1.POST("/register", controller.StudentRegisterHandler)
 	}
 	router.Run() // 监听并在 0.0.0.0:8080 上启动服务
 }
--- server/model/student.go
+++ server/model/student.go
@@ -1,6 +1,10 @@
 package model
 
-import _ "github.com/go-sql-driver/mysql"
+import (
+	"ccs/db"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+)
 
 type Student struct {
 	Id int64 `json:"id"`
@@ -11,9 +15,29 @@ type Student struct {
 
 func (s *Student) UserIsExists() bool {
 	sql := "select id from students where id=?"
-
+	var id int64
+	_ = db.DB.QueryRow(sql, s.Id).Scan(&id)
+	if id > 0 {
+		return true
+	}
+	return false
 }
 
 func (s *Student) Add () {
-	sql := ""
+	sql := "insert into students values(?,?,?,?)"
+	_, err := db.DB.Exec(sql, s.Id, s.Name, s.Password, s.CId)
+	if err != nil {
+		log.Println(err.Error())
+	}
 }
+
+func (s *Student) CheckPassword() bool {
+	sql := "select password from student where id=?"
+	var password string
+	_ = db.DB.QueryRow(sql, s.Id).Scan(&password)
+	if s.Password == password {
+		return true
+	}
+	return false
+}
+
