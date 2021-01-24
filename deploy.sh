5d73cb4226c4d32509624afdaa4e933fc63787bf
tankz <1785619665@qq.com>
2021-01-24 19:15:59 +0800

    [modify]]

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,31 @@
 #!/bin/sh
+######### 安装docker ###########
 if ! type docker >/dev/null 2>&1
-then 
-    sudo apt-get remove docker docker-engine docker.io
-    sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-    sudo apt-get update
-    $ curl -fsSL https://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository \
+then
+    apt-get remove docker docker-engine docker.io
+    apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+    apt-get update
+    $ curl -fsSL https://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg |  apt-key add -
+    add-apt-repository \
     "deb [arch=amd64] https://mirrors.aliyun.com/docker-ce/linux/ubuntu \
     $(lsb_release -cs) \
     stable"
-    sudo apt-get update
-    sudo apt-get install docker-ce docker-ce-cli containerd.io
-    sudo systemctl enable docker
-    sudo systemctl start docker
+    apt-get update
+    apt-get install docker-ce docker-ce-cli containerd.io
+    systemctl enable docker
+    systemctl start docker
+    echo 'docker install successfully'
 else
-    sudo apt-get update
-    echo 'test jenkins'
-    echo 'hello world'
+    echo 'docker is installed'
 fi
+
+####### 安装docker-compose ######
+if !type docker-compose >/dev/null 2>&1
+then
+    curl -L https://download.fastgit.org/docker/compose/releases/download/1.27.4/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+else
+    echo 'docker-compose is installed'
+fi
+
+docker-compose up -d
