1fa7036cd44cbb3b5d261d048e873e8344277c4e
tankz <1785619665@qq.com>
2021-03-02 22:44:43 +0800

    [modify] 添加提示信息

--- deploy.sh
+++ deploy.sh
@@ -17,8 +17,11 @@ echo "{\"registry-mirrors\":[\"https://mirror.ccs.tencentyun.com\"]}" > /etc/doc
 sudo systemctl daemon-reload
 sudo systemctl restart docker
 sudo docker build -t ccs .
+echo "docker build successfully"
 sudo docker run -d --privileged=true --name=ccs -p 4200:4200 ccs
+echo "docker run successfully"
 sudo sleep 5
+echo "docker-compose up..."
 sudo docker exec -it ccs /bin/sh -c "docker-compose up -d"
 echo "部署完成,请访问127.0.0.1:4200进行预览!"
 set +e
