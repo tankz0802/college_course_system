bc30938d7a6e0789be032718bfee26c1696ed9d6
tankz <1785619665@qq.com>
2021-03-02 22:39:54 +0800

    [modify] 使用脚本安装docker

--- deploy.sh
+++ deploy.sh
@@ -3,36 +3,8 @@ set -e
 ######### 安装docker ###########
 if ! type docker >/dev/null 2>&1
 then
-######### 配置阿里镜像源 ########
-sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
-cat>/etc/apt/sources.list<<EOF
-deb http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
-EOF
-sudo apt-get update
-    sudo apt-get remove docker docker-engine docker.io
-    sudo apt-get update
-    sudo apt-get -y install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
-    sudo curl -fsSL https://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg |  apt-key add -
-    add-apt-repository \
-    "deb [arch=amd64] https://mirrors.aliyun.com/docker-ce/linux/ubuntu \
-    $(lsb_release -cs) \
-    stable"
-    sudo apt-get update
-    sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+    curl -fsSL get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh --mirror Aliyun
     sudo systemctl enable docker
     sudo systemctl start docker
     echo 'docker install successfully'
