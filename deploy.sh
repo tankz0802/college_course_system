d188b1469def0aa9878b3e12d96cb39ccac0165a
tankz <1785619665@qq.com>
2021-03-02 22:24:07 +0800

    [modify] 修改睡眠时间

--- deploy.sh
+++ deploy.sh
@@ -46,7 +46,7 @@ sudo systemctl daemon-reload
 sudo systemctl restart docker
 sudo docker build -t ccs .
 sudo docker run -d --privileged=true --name=ccs -p 4200:4200 ccs
-sudo sleep 3
+sudo sleep 5
 sudo docker exec -it ccs /bin/sh -c "docker-compose up -d"
 echo "部署完成,请访问127.0.0.1:4200进行预览!"
 set +e
